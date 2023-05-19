b56b52799e8558591b5822db2af2635e623a3c7b
Carlos-77Av <carlos.av.bit@gmail.com>
2023-05-18 18:19:13 -0600

    🎉 - Initial project

---
+++ MarvelFanAV/MarvelFanAV/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MarvelFanAV
+//
+//  Created by Carlos Alvarez on 18/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarvelFanAV/MarvelFanAV/MarvelFanAVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelFanAVApp.swift
+//  MarvelFanAV
+//
+//  Created by Carlos Alvarez on 18/5/23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelFanAVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
